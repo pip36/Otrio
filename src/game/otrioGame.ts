53581efba369c7d75aa9c63ce695fae3528962ad
Philip White <philip.white@redington.co.uk>
2025-09-05 11:54:38 +0100

    Rules and win condition implemented

--- src/game/otrioGame.ts
+++ src/game/otrioGame.ts
@@ -10,6 +10,7 @@ export type Slot = {
 };
 
 export type Board = {
+  winningPlayer: Piece["color"] | null;
   currentTurn: Piece["color"];
   slots: [Slot, Slot, Slot, Slot, Slot, Slot, Slot, Slot, Slot];
   remainingPiecesByPlayer: {
@@ -17,7 +18,7 @@ export type Board = {
   };
 };
 
-const startingSlots: Board["slots"] = [
+const startingSlots = (): Board["slots"] => [
   { small: null, medium: null, large: null },
   { small: null, medium: null, large: null },
   { small: null, medium: null, large: null },
@@ -29,48 +30,96 @@ const startingSlots: Board["slots"] = [
   { small: null, medium: null, large: null },
 ];
 
-const startingPiecesByPlayer: Board["remainingPiecesByPlayer"] = {
+const startingPiecesByPlayer = (): Board["remainingPiecesByPlayer"] => ({
   red: [
     { color: "red", size: "small" },
+    { color: "red", size: "small" },
+    { color: "red", size: "small" },
     { color: "red", size: "medium" },
+    { color: "red", size: "medium" },
+    { color: "red", size: "medium" },
+    { color: "red", size: "large" },
+    { color: "red", size: "large" },
     { color: "red", size: "large" },
   ],
   blue: [
     { color: "blue", size: "small" },
+    { color: "blue", size: "small" },
+    { color: "blue", size: "small" },
     { color: "blue", size: "medium" },
+    { color: "blue", size: "medium" },
+    { color: "blue", size: "medium" },
+    { color: "blue", size: "large" },
+    { color: "blue", size: "large" },
     { color: "blue", size: "large" },
   ],
   yellow: [
     { color: "yellow", size: "small" },
+    { color: "yellow", size: "small" },
+    { color: "yellow", size: "small" },
     { color: "yellow", size: "medium" },
+    { color: "yellow", size: "medium" },
+    { color: "yellow", size: "medium" },
+    { color: "yellow", size: "large" },
+    { color: "yellow", size: "large" },
     { color: "yellow", size: "large" },
   ],
   green: [
     { color: "green", size: "small" },
+    { color: "green", size: "small" },
+    { color: "green", size: "small" },
+    { color: "green", size: "medium" },
+    { color: "green", size: "medium" },
     { color: "green", size: "medium" },
     { color: "green", size: "large" },
+    { color: "green", size: "large" },
+    { color: "green", size: "large" },
   ],
-};
+});
 
 export class OtrioGame {
   private piecesByPlayer: Board["remainingPiecesByPlayer"] =
-    startingPiecesByPlayer;
+    startingPiecesByPlayer();
 
-  private slots: Board["slots"] = startingSlots;
+  private slots: Board["slots"] = startingSlots();
   private currentTurn: Piece["color"] = "red";
+  private winningPlayer: Piece["color"] | null = null;
 
   public getBoard(): Board {
     return {
       slots: this.slots,
       remainingPiecesByPlayer: this.piecesByPlayer,
       currentTurn: this.currentTurn,
+      winningPlayer: this.winningPlayer,
     };
   }
 
   public placePiece(
-    _size: "small" | "medium" | "large",
-    _location: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
+    size: "small" | "medium" | "large",
+    location: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
   ): void {
+    const pieceToPlayIndex = this.piecesByPlayer[this.currentTurn].findIndex(
+      (piece) => piece.size === size
+    );
+
+    if (pieceToPlayIndex === -1) {
+      // Player does not have any pieces of that size left
+      return;
+    }
+
+    if (this.slots[location][size] !== null) {
+      // Slot already occupied
+      return;
+    }
+
+    // Remove the piece from the player's available pieces
+    this.piecesByPlayer[this.currentTurn].splice(pieceToPlayIndex, 1);
+
+    // Place the piece on the board
+    this.slots[location][size] = { color: this.currentTurn, size };
+
+    this.detectWin();
+
     this.setNextPlayerTurn();
   }
 
@@ -84,5 +133,97 @@ export class OtrioGame {
 
     const nextPlayersTurn = getNextTurn[this.currentTurn];
     this.currentTurn = nextPlayersTurn;
+  }
+
+  private detectWin(): void {
+    // Three of same color in one slot
+    for (const slot of this.slots) {
+      if (
+        slot.small?.color &&
+        slot.medium?.color &&
+        slot.large?.color &&
+        slot.small.color === slot.medium.color &&
+        slot.medium.color === slot.large.color
+      ) {
+        this.winningPlayer = slot.small.color;
+        return;
+      }
+    }
+
+    // Three of same size in a row/column/diagonal
+    const linesToCheck: [number, number, number][] = [
+      // Rows
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      // Columns
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      // Diagonals
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (const [a, b, c] of linesToCheck) {
+      const slotA = this.slots[a];
+      const slotB = this.slots[b];
+      const slotC = this.slots[c];
+
+      if (
+        slotA.small?.color &&
+        slotB.small?.color &&
+        slotC.small?.color &&
+        slotA.small.color === slotB.small.color &&
+        slotB.small.color === slotC.small.color
+      ) {
+        this.winningPlayer = slotA.small.color;
+        return;
+      }
+
+      if (
+        slotA.medium?.color &&
+        slotB.medium?.color &&
+        slotC.medium?.color &&
+        slotA.medium.color === slotB.medium.color &&
+        slotB.medium.color === slotC.medium.color
+      ) {
+        this.winningPlayer = slotA.medium.color;
+        return;
+      }
+
+      if (
+        slotA.large?.color &&
+        slotB.large?.color &&
+        slotC.large?.color &&
+        slotA.large.color === slotB.large.color &&
+        slotB.large.color === slotC.large.color
+      ) {
+        this.winningPlayer = slotA.large.color;
+        return;
+      }
+
+      if (
+        slotA.small?.color &&
+        slotB.medium?.color &&
+        slotC.large?.color &&
+        slotA.small.color === slotB.medium.color &&
+        slotB.medium.color === slotC.large.color
+      ) {
+        this.winningPlayer = slotA.small.color;
+        return;
+      }
+
+      if (
+        slotA.large?.color &&
+        slotB.medium?.color &&
+        slotC.small?.color &&
+        slotA.large.color === slotB.medium.color &&
+        slotB.medium.color === slotC.small.color
+      ) {
+        this.winningPlayer = slotA.large.color;
+        return;
+      }
+    }
   }
 }
--- tests/otrioGame.test.ts
+++ tests/otrioGame.test.ts
@@ -20,22 +20,46 @@ describe("OtrioGame", () => {
     expect(board.remainingPiecesByPlayer).toEqual({
       red: [
         { color: "red", size: "small" },
+        { color: "red", size: "small" },
+        { color: "red", size: "small" },
         { color: "red", size: "medium" },
+        { color: "red", size: "medium" },
+        { color: "red", size: "medium" },
+        { color: "red", size: "large" },
+        { color: "red", size: "large" },
         { color: "red", size: "large" },
       ],
       blue: [
         { color: "blue", size: "small" },
+        { color: "blue", size: "small" },
+        { color: "blue", size: "small" },
         { color: "blue", size: "medium" },
+        { color: "blue", size: "medium" },
+        { color: "blue", size: "medium" },
+        { color: "blue", size: "large" },
+        { color: "blue", size: "large" },
         { color: "blue", size: "large" },
       ],
       yellow: [
         { color: "yellow", size: "small" },
+        { color: "yellow", size: "small" },
+        { color: "yellow", size: "small" },
         { color: "yellow", size: "medium" },
+        { color: "yellow", size: "medium" },
+        { color: "yellow", size: "medium" },
+        { color: "yellow", size: "large" },
+        { color: "yellow", size: "large" },
         { color: "yellow", size: "large" },
       ],
       green: [
         { color: "green", size: "small" },
+        { color: "green", size: "small" },
+        { color: "green", size: "small" },
         { color: "green", size: "medium" },
+        { color: "green", size: "medium" },
+        { color: "green", size: "medium" },
+        { color: "green", size: "large" },
+        { color: "green", size: "large" },
         { color: "green", size: "large" },
       ],
     });
@@ -64,5 +88,272 @@ describe("OtrioGame", () => {
 
     game.placePiece("small", 3);
     expect(game.getBoard().currentTurn).toBe("red");
+  });
+
+  it("should place small pieces of correct color onto board", () => {
+    const game = new OtrioGame();
+
+    game.placePiece("small", 0);
+    game.placePiece("small", 1);
+    game.placePiece("small", 2);
+    game.placePiece("small", 3);
+    expect(game.getBoard().slots).toEqual([
+      { small: { color: "red", size: "small" }, medium: null, large: null },
+      { small: { color: "blue", size: "small" }, medium: null, large: null },
+      { small: { color: "yellow", size: "small" }, medium: null, large: null },
+      { small: { color: "green", size: "small" }, medium: null, large: null },
+      { small: null, medium: null, large: null },
+      { small: null, medium: null, large: null },
+      { small: null, medium: null, large: null },
+      { small: null, medium: null, large: null },
+      { small: null, medium: null, large: null },
+    ]);
+  });
+
+  it("should do nothing if you try to place a piece in an already taken slot", () => {
+    const game = new OtrioGame();
+
+    game.placePiece("small", 0);
+    game.placePiece("small", 0); // blue tries to play in the same slot
+    expect(game.getBoard().slots).toEqual([
+      { small: { color: "red", size: "small" }, medium: null, large: null },
+      { small: null, medium: null, large: null },
+      { small: null, medium: null, large: null },
+      { small: null, medium: null, large: null },
+      { small: null, medium: null, large: null },
+      { small: null, medium: null, large: null },
+      { small: null, medium: null, large: null },
+      { small: null, medium: null, large: null },
+      { small: null, medium: null, large: null },
+    ]);
+    expect(game.getBoard().currentTurn).toBe("blue"); // turn should not change
+  });
+
+  it("should remove played pieces from player's pool", () => {
+    const game = new OtrioGame();
+
+    game.placePiece("small", 0);
+    expect(game.getBoard().remainingPiecesByPlayer.red).toEqual([
+      { color: "red", size: "small" },
+      { color: "red", size: "small" },
+      { color: "red", size: "medium" },
+      { color: "red", size: "medium" },
+      { color: "red", size: "medium" },
+      { color: "red", size: "large" },
+      { color: "red", size: "large" },
+      { color: "red", size: "large" },
+    ]);
+
+    game.placePiece("medium", 0);
+    expect(game.getBoard().remainingPiecesByPlayer.blue).toEqual([
+      { color: "blue", size: "small" },
+      { color: "blue", size: "small" },
+      { color: "blue", size: "small" },
+      { color: "blue", size: "medium" },
+      { color: "blue", size: "medium" },
+      { color: "blue", size: "large" },
+      { color: "blue", size: "large" },
+      { color: "blue", size: "large" },
+    ]);
+
+    game.placePiece("large", 0);
+    expect(game.getBoard().remainingPiecesByPlayer.yellow).toEqual([
+      { color: "yellow", size: "small" },
+      { color: "yellow", size: "small" },
+      { color: "yellow", size: "small" },
+      { color: "yellow", size: "medium" },
+      { color: "yellow", size: "medium" },
+      { color: "yellow", size: "medium" },
+      { color: "yellow", size: "large" },
+      { color: "yellow", size: "large" },
+    ]);
+
+    game.placePiece("large", 1);
+    expect(game.getBoard().remainingPiecesByPlayer.green).toEqual([
+      { color: "green", size: "small" },
+      { color: "green", size: "small" },
+      { color: "green", size: "small" },
+      { color: "green", size: "medium" },
+      { color: "green", size: "medium" },
+      { color: "green", size: "medium" },
+      { color: "green", size: "large" },
+      { color: "green", size: "large" },
+    ]);
+
+    game.placePiece("small", 1);
+    expect(game.getBoard().remainingPiecesByPlayer.red).toEqual([
+      { color: "red", size: "small" },
+      { color: "red", size: "medium" },
+      { color: "red", size: "medium" },
+      { color: "red", size: "medium" },
+      { color: "red", size: "large" },
+      { color: "red", size: "large" },
+      { color: "red", size: "large" },
+    ]);
+  });
+
+  it("should detect a win for a stack of 3 matching colors in the same slot", () => {
+    const game = new OtrioGame();
+
+    game.placePiece("small", 0); // small red in slot 0
+    game.placePiece("small", 1);
+    game.placePiece("small", 2);
+    game.placePiece("small", 3);
+
+    game.placePiece("medium", 0); // medium red in slot 0
+    game.placePiece("medium", 1);
+    game.placePiece("medium", 2);
+    game.placePiece("medium", 3);
+
+    game.placePiece("large", 0); // large red in slot 0 - should win here
+    const board = game.getBoard();
+    expect(board.winningPlayer).toBe("red");
+  });
+
+  it("should detect a win for a horizontal line of 3 matching colors of same size", () => {
+    const game = new OtrioGame();
+
+    game.placePiece("small", 0); // small red in slot 0
+    game.placePiece("small", 3);
+    game.placePiece("small", 4);
+    game.placePiece("small", 5);
+
+    game.placePiece("small", 1); // small red in slot 1
+    game.placePiece("medium", 3);
+    game.placePiece("medium", 4);
+    game.placePiece("medium", 5);
+
+    game.placePiece("small", 2); // small red in slot 2 - should win here
+
+    const board = game.getBoard();
+    expect(board.winningPlayer).toBe("red");
+  });
+
+  it("should detect a win for a diagonal line of 3 matching colors of same size", () => {
+    const game = new OtrioGame();
+
+    game.placePiece("medium", 0); // medium red in slot 0
+    game.placePiece("small", 3);
+    game.placePiece("small", 2);
+    game.placePiece("small", 5);
+
+    game.placePiece("medium", 4); // medium red in slot 4
+    game.placePiece("medium", 3);
+    game.placePiece("medium", 7);
+    game.placePiece("medium", 5);
+
+    game.placePiece("medium", 8); // medium red in slot 8 - should win here
+
+    const board = game.getBoard();
+    expect(board.winningPlayer).toBe("red");
+  });
+
+  it("should detect a win for a vertical line of 3 matching colors of same size", () => {
+    const game = new OtrioGame();
+
+    game.placePiece("large", 0); // large red in slot 0
+    game.placePiece("small", 3);
+    game.placePiece("small", 2);
+    game.placePiece("small", 5);
+
+    game.placePiece("large", 3); // large red in slot 3
+    game.placePiece("medium", 3);
+    game.placePiece("medium", 7);
+    game.placePiece("medium", 5);
+
+    game.placePiece("large", 6); // large red in slot 6 - should win here
+
+    const board = game.getBoard();
+    expect(board.winningPlayer).toBe("red");
+  });
+
+  it("should detect a win for a horizontal line of 3 matching colors in size order ascending", () => {
+    const game = new OtrioGame();
+
+    game.placePiece("small", 0); // small red in slot 0
+    game.placePiece("small", 3);
+    game.placePiece("small", 2);
+    game.placePiece("small", 5);
+
+    game.placePiece("medium", 1); // medium red in slot 1
+    game.placePiece("medium", 3);
+    game.placePiece("medium", 7);
+    game.placePiece("medium", 5);
+
+    game.placePiece("large", 2); // large red in slot 2 - should win here
+
+    const board = game.getBoard();
+    expect(board.winningPlayer).toBe("red");
+  });
+
+  it("should detect a win for a horizontal line of 3 matching colors in size order descending", () => {
+    const game = new OtrioGame();
+
+    game.placePiece("large", 0); // large red in slot 0
+    game.placePiece("small", 3);
+    game.placePiece("small", 4);
+    game.placePiece("small", 5);
+
+    game.placePiece("medium", 1); // medium red in slot 1
+    game.placePiece("medium", 3);
+    game.placePiece("medium", 7);
+    game.placePiece("medium", 5);
+
+    game.placePiece("small", 2); // small red in slot 2 - should win here
+
+    const board = game.getBoard();
+    expect(board.winningPlayer).toBe("red");
+  });
+
+  it("should do nothing if player does not have any of that piece left", () => {
+    const game = new OtrioGame();
+    game.placePiece("small", 0); // red plays a small
+    game.placePiece("medium", 1); // blue
+    game.placePiece("medium", 2); // yellow
+    game.placePiece("medium", 3); // green
+    expect(game.getBoard().currentTurn).toEqual("red");
+
+    game.placePiece("small", 4); // red plays another small
+    game.placePiece("medium", 5); // blue
+    game.placePiece("medium", 6); // yellow
+    game.placePiece("medium", 7); // green
+    expect(game.getBoard().currentTurn).toEqual("red");
+
+    game.placePiece("small", 8); // red plays their last small
+    game.placePiece("large", 0); // blue
+    game.placePiece("large", 1); // yellow
+    game.placePiece("large", 2); // green
+    expect(game.getBoard().currentTurn).toEqual("red");
+
+    game.placePiece("small", 7); // Should do nothing - red has no smalls left
+
+    expect(game.getBoard().currentTurn).toEqual("red");
+    expect(game.getBoard().slots).toEqual([
+      {
+        small: { color: "red", size: "small" },
+        medium: null,
+        large: { color: "blue", size: "large" },
+      },
+      {
+        small: null,
+        medium: { color: "blue", size: "medium" },
+        large: { color: "yellow", size: "large" },
+      },
+      {
+        small: null,
+        medium: { color: "yellow", size: "medium" },
+        large: { color: "green", size: "large" },
+      },
+      {
+        small: null,
+        medium: { color: "green", size: "medium" },
+        large: null,
+      },
+      { small: { color: "red", size: "small" }, medium: null, large: null },
+      { small: null, medium: { color: "blue", size: "medium" }, large: null },
+      { small: null, medium: { color: "yellow", size: "medium" }, large: null },
+      { small: null, medium: { color: "green", size: "medium" }, large: null },
+      { small: { color: "red", size: "small" }, medium: null, large: null },
+    ]);
   });
 });
